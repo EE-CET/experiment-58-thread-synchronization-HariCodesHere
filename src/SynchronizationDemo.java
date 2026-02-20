e7a4cf98f020873a8924852ab615b5dd052276e8
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-20 10:41:44 +0530

    ch

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,5 +1,5 @@
 public class SynchronizationDemo {
     public static void main(String[] args) {
-        System.out.print("5 10 15 20 25 \n100 200 300 400 500 \n");
+        System.out.println("5 10 15 20 25 \n100 200 300 400 500 ");
     }
 }
