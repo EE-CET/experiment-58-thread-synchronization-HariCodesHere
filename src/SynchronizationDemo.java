7f3ade58e92923dbaea6782044a408f1c12dd537
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-20 10:24:44 +0530

    order check

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -2,14 +2,8 @@ class Table {
     synchronized void printTable(int n) {
         for (int i = 1; i <= 5; i++) {
             System.out.print(n * i + " ");
-            try {
-                Thread.sleep(400);
-            } catch (InterruptedException e) {
-            }
         }
-        if (n == 5) {
-            System.out.print("\n");
-        }
+        System.out.print("\n");
     }
 }
 
@@ -34,15 +28,14 @@ class MyThread2 extends Thread {
 }
 
 public class SynchronizationDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Table t = new Table();
         MyThread1 t1 = new MyThread1(t);
         MyThread2 t2 = new MyThread2(t);
+
         t1.start();
-        try {
-            t1.join();
-        } catch (InterruptedException e) {
-        }
+        t1.join();
         t2.start();
+        t2.join();
     }
 }
