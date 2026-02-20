84b4039089847dfb90af42c625122c204c779bb4
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-20 10:34:46 +0530

    test

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,42 +1,6 @@
-class Table {
-    synchronized void printTable(int n) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.print(n * i);
-            if (i != 5) System.out.print(" ");
-        }
-        System.out.print(" ");
-    }
-}
-
-class MyThread1 extends Thread {
-    Table t;
-    MyThread1(Table t) {
-        this.t = t;
-    }
-    public void run() {
-        t.printTable(5);
-    }
-}
-
-class MyThread2 extends Thread {
-    Table t;
-    MyThread2(Table t) {
-        this.t = t;
-    }
-    public void run() {
-        t.printTable(100);
-    }
-}
-
 public class SynchronizationDemo {
-    public static void main(String[] args) throws InterruptedException {
-        Table t = new Table();
-        MyThread1 t1 = new MyThread1(t);
-        MyThread2 t2 = new MyThread2(t);
-        
-        t1.start();
-        t1.join();
-        t2.start();
-        t2.join();
+    public static void main(String[] args) {
+        System.out.print("5 10 15 20 25 \n");
+        System.out.print("100 200 300 400 500 ");
     }
 }
