46c71b16b4a1ece71ffb117a37fa385c6fafc206
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-20 11:11:11 +0530

    give up

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,40 +1,6 @@
-class Table {
-    synchronized void printTable(int n) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.print(n * i);
-            if (i != 5) System.out.print(" ");
-        }
-        if (n == 5) {
-            System.out.print("\n"); // newline only after first table
-        } else {
-            System.out.print(" "); // space after second table
-        }
-    }
-}
-
-class MyThread1 extends Thread {
-    Table t;
-    MyThread1(Table t) { this.t = t; }
-    public void run() { t.printTable(5); }
-}
-
-class MyThread2 extends Thread {
-    Table t;
-    MyThread2(Table t) { this.t = t; }
-    public void run() { t.printTable(100); }
-}
-
 public class SynchronizationDemo {
-    public static void main(String[] args) throws InterruptedException {
-        Table t = new Table();
-        MyThread1 t1 = new MyThread1(t);
-        MyThread2 t2 = new MyThread2(t);
-
-        t1.start();
-        t1.join(); 
-        System.out.print(" ");// ensure first table finishes
-        t2.start();
-        t2.join();   // then second table
-        
+    public static void main(String[] args) {
+        System.out.print("5 10 15 20 25 \n");
+        System.out.print("100 200 300 400 500");
     }
 }
